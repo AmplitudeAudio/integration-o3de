1b4c07636c6125ed3f3b7160536a22db215c5acd
Axel Nana <axel.nana@aliens-group.com>
2022-03-20 01:40:39 +0100

    fix: Add libsamplerate static library

--- 3rdParty/Platform/Linux/AmplitudeAudioSDK_linux.cmake
+++ 3rdParty/Platform/Linux/AmplitudeAudioSDK_linux.cmake
@@ -1,1 +1,4 @@
-set(AMPLITUDEAUDIOSDK_LIBS ${BASE_PATH}/sdk/lib/win/$<IF:$<CONFIG:Debug>,libAmplitude_d.so,libAmplitude.so>)
+set(AMPLITUDEAUDIOSDK_LIBS
+    ${BASE_PATH}/sdk/lib/win/$<IF:$<CONFIG:Debug>,libAmplitude_d.so,libAmplitude.so>
+    ${BASE_PATH}/sdk/lib/win/$<IF:$<CONFIG:Debug>,libsamplerate_d.so,libsamplerate.so>
+)
--- 3rdParty/Platform/Windows/AmplitudeAudioSDK_windows.cmake
+++ 3rdParty/Platform/Windows/AmplitudeAudioSDK_windows.cmake
@@ -1,1 +1,4 @@
-set(AMPLITUDEAUDIOSDK_LIBS ${BASE_PATH}/sdk/lib/win/$<IF:$<CONFIG:Debug>,Amplitude_d.lib,Amplitude.lib>)
+set(AMPLITUDEAUDIOSDK_LIBS
+    ${BASE_PATH}/sdk/lib/win/$<IF:$<CONFIG:Debug>,Amplitude_d.lib,Amplitude.lib>
+    ${BASE_PATH}/sdk/lib/win/$<IF:$<CONFIG:Debug>,samplerate_d.lib,samplerate.lib>
+)
