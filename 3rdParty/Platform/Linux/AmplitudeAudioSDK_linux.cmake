9757a82bcec3e9301f37e9e6e975a16992c8dce1
Nana Axel <axel.nana@aliens-group.com>
2022-03-21 15:58:42 +0100

    fix: Libraries path on Linux platforms

--- 3rdParty/Platform/Linux/AmplitudeAudioSDK_linux.cmake
+++ 3rdParty/Platform/Linux/AmplitudeAudioSDK_linux.cmake
@@ -1,4 +1,4 @@
 set(AMPLITUDEAUDIOSDK_LIBS
-    ${BASE_PATH}/sdk/lib/win/$<IF:$<CONFIG:Debug>,libAmplitude_d.so,libAmplitude.so>
-    ${BASE_PATH}/sdk/lib/win/$<IF:$<CONFIG:Debug>,libsamplerate_d.so,libsamplerate.so>
+    ${BASE_PATH}/sdk/lib/linux/$<IF:$<CONFIG:Debug>,libAmplitude_d.a,libAmplitude.a>
+    ${BASE_PATH}/sdk/lib/linux/$<IF:$<CONFIG:Debug>,libsamplerate_d.a,libsamplerate.a>
 )
